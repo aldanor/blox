b37d36a1da7d775ccb7c6fec4c0fbe98ab49b037
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-10-28 21:34:51 +0000

    Package everything properly; add version information

--- blox/__init__.py
+++ blox/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from blox.file import File
+from blox._version import __version__
 
-__all__ = ('File',)
+__all__ = ('File', '__version__')
--- setup.py
+++ setup.py
@@ -1,9 +1,42 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+import io
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = ''
+with io.open('blox/_version.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
 
 setup(
     name='blox',
+    version=version,
+    description='Fast binary data storage with blosc support.',
+    author='Ivan Smirnov',
+    author_email='i.s.smirnov@gmail.com',
+    url='https://github.com/aldanor/blox',
+    license='MIT',
     packages=['blox'],
-    install_requires=['numpy', 'blosc']
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5',
+    ),
+    install_requires=[
+        'numpy', 'blosc'
+    ],
+    extras_require={
+        'extras': ['ujson']
+    }
 )
