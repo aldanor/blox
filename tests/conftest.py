8b3a5bd9c28ba15e82215d4410b2952bcc81b917
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-10-28 21:17:35 +0000

    Create a valid blox file in tmpfile fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,11 @@
 
 import pytest
 
+from blox.file import File
+
 
 @pytest.yield_fixture
 def tmpfile(request, tmpdir):
-    yield tmpdir.join('file.tmp').ensure().strpath
+    filename = tmpdir.join('file.tmp').ensure().strpath
+    File(filename, 'w').close()
+    yield filename
