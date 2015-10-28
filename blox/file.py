283160e6acad433b6807090d4869295698209b35
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-10-28 21:21:32 +0000

    Minor refactoring only

--- blox/file.py
+++ blox/file.py
@@ -24,12 +24,7 @@ class File(object):
         self._dirty = True
         atexit.register(self.close)
         if not self.writable:
-            try:
-                self._handle.seek(-8, os.SEEK_END)
-                self._handle.seek(read_i64(self._handle))
-                self._index = read_json(self._handle)
-            except Exception as e:
-                raise IOError('cannot read file ({}: {})'.format(type(e).__name__, str(e)))
+            self._read_index()
         self._seek = 0
 
     @property
@@ -48,17 +43,15 @@ class File(object):
     def filesize(self):
         return os.stat(self._filename).st_size
 
-    def _write_index(self):
-        self._handle.truncate()
-        write_json(self._handle, self._index)
-        write_i64(self._handle, self._seek)
-        self._dirty = False
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
+    def create_dataset(self, name, data, compression='lz4', level=5, shuffle=True):
+        self._check_handle(write=True)
+        if name in self._index:
+            raise ValueError('dataset {!r} already exists'.format(name))
+        self._index[name] = self._seek
+        self._handle.seek(self._seek)
+        self._dirty = True
+        self._seek = write_blosc(data, self._handle, compression, level, shuffle)
+        self._write_index()
 
     def close(self):
         if self._handle is not None:
@@ -69,18 +62,28 @@ class File(object):
             self._handle.close()
         self._handle = None
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def _read_index(self):
+        try:
+            self._handle.seek(-8, os.SEEK_END)
+            self._handle.seek(read_i64(self._handle))
+            self._index = read_json(self._handle)
+        except Exception as e:
+            raise IOError('cannot read file ({}: {})'.format(type(e).__name__, str(e)))
+
+    def _write_index(self):
+        self._handle.truncate()
+        write_json(self._handle, self._index)
+        write_i64(self._handle, self._seek)
+        self._dirty = False
+
     def _check_handle(self, write=False):
         if self._handle is None:
             raise IOError('the file handle has been closed')
         if write and not self.writable:
             raise IOError('the file is not writable')
-
-    def create_dataset(self, name, data, compression='lz4', level=5, shuffle=True):
-        self._check_handle(write=True)
-        if name in self._index:
-            raise ValueError('dataset {!r} already exists'.format(name))
-        self._index[name] = self._seek
-        self._handle.seek(self._seek)
-        self._dirty = True
-        self._seek = write_blosc(data, self._handle, compression, level, shuffle)
-        self._write_index()
